ded9914183f9c39b1ed68d091c78222a5b5f080e
Dan Pozmanter <dan.pozmanter@venuebook.com>
2016-01-11 18:04:21 -0500

    Setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+    name='PyCronofy',
+    version='0.0.0',
+    description='Python library for Cronofy',
+    author='VenueBook',
+    author_email='dev@venuebook.com',
+    url='',
+    packages=['pycronofy']
+    )
