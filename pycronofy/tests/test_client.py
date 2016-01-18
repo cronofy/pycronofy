5a63022acd9ce324f5d706d37fac25cb1e1a0fef
Dan Pozmanter <dan.pozmanter@venuebook.com>
2016-01-17 23:52:58 -0500

    Increasing coverage, refactoring

--- pycronofy/tests/test_client.py
+++ pycronofy/tests/test_client.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 import pytest
 import responses
 import requests
@@ -6,27 +5,82 @@ from ..client import CronofyClient
 from .. import settings
 import test_data
 
+TEST_EVENT = {
+    'event_id': 'test-1',
+    'summary': 'Test Event',
+    'description': 'Talk about how awesome cats are.',
+    'start': '2014-10-01T08:00:00Z',
+    'end': '2014-10-01T09:00:00Z',
+    'tzid': 'Etc/UTC',
+    'location': {
+        'description': 'Location!',
+    },
+}
+
+TEST_CREATE_NOTIFICATION_ARGS = { 
+    'method': responses.POST,
+    'url': '%s/%s/channels' % (settings.API_BASE_URL, settings.API_VERSION),
+    'body': '{"example": 1}',
+    'status': 200,
+    'content_type':'application/json'
+}
+
+TEST_EVENTS_ARGS = { 
+    'url': '%s/%s/events' % (settings.API_BASE_URL, settings.API_VERSION),
+    'body': '{"example": 1}',
+    'status': 200,
+    'content_type':'application/json'
+}
+
+TEST_UPSERT_EVENT_ARGS = { 
+    'method': responses.POST,
+    'url': '%s/%s/calendars/1/events' % (settings.API_BASE_URL, settings.API_VERSION),
+    'body': '{"example": 1}',
+    'status': 200,
+    'content_type':'application/json'
+}
+
 @pytest.fixture(scope="module")
 def client():
     """Setup Client instance with test values."""
     return CronofyClient(**test_data.AUTH_ARGS)
 
 @responses.activate
-def test__get(client):
-    """Test CronofyClient.__get().
+def test_create_notification_channel(client):
+    """Test CronofyClient.create_notification_channel().
 
     :param CronofyClient client: CronofyClient instance with test data.
     """
-    responses.add(method=responses.GET, **test_data.TEST_EVENTS_ARGS)
+    responses.add(**TEST_CREATE_NOTIFICATION_ARGS)
+    response = client.create_notification_channel('http://example.com', calendar_ids=('1',))
+    assert response.status_code == requests.codes.ok
+
+@responses.activate
+def test_upsert_event(client):
+    """Test CronofyClient.upsert_event().
+
+    :param CronofyClient client: CronofyClient instance with test data.
+    """
+    responses.add(**TEST_UPSERT_EVENT_ARGS)
+    response = client.upsert_event('1', TEST_EVENT)
+    assert response.status_code == requests.codes.ok
+
+@responses.activate
+def test__get(client):
+    """Test CronofyClient._get().
+
+    :param CronofyClient client: CronofyClient instance with test data.
+    """
+    responses.add(method=responses.GET, **TEST_EVENTS_ARGS)
     response = client._get(endpoint='events')
     assert response['example'] == 1
 
 @responses.activate
 def test__post(client):
-    """Test CronofyClient.__post().
+    """Test CronofyClient._post().
 
     :param CronofyClient client: CronofyClient instance with test data.
     """
-    responses.add(method=responses.POST, **test_data.TEST_EVENTS_ARGS)
+    responses.add(method=responses.POST, **TEST_EVENTS_ARGS)
     response = client._post(endpoint='events')
     assert response.status_code == requests.codes.ok
--- pycronofy/tests/test_pagination.py
+++ pycronofy/tests/test_pagination.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import json
 import pytest
 import responses
 import requests
@@ -6,6 +7,84 @@ from ..client import CronofyClient
 from ..pagination import Pages
 from .. import settings
 import test_data
+
+TEST_DATA_PAGE_ONE = {
+  "pages": {
+    "current": 1,
+    "total": 2,
+    "next_page": "%s/%s/events/pages/08a07b034306679e" % (settings.API_BASE_URL, settings.API_VERSION)
+  },
+  "events": [
+    {
+      "calendar_id": "cal_U9uuErStTG@EAAAB_IsAsykA2DBTWqQTf-f0kJw",
+      "event_uid": "evt_external_54008b1a4a41730f8d5c6037",
+      "summary": "Company Retreat",
+      "description": "",
+      "start": "2014-09-06",
+      "end": "2014-09-08",
+      "deleted": False,
+      "location": {
+        "description": "Beach"
+      },
+      "participation_status": "needs_action",
+      "transparency": "opaque",
+      "event_status": "confirmed",
+      "categories": [],
+      "attendees": [
+        {
+          "email": "example@cronofy.com",
+          "display_name": "Example Person",
+          "status": "needs_action"
+        }
+      ],
+      "created": "2014-09-01T08:00:01Z",
+      "updated": "2014-09-01T09:24:16Z"
+    }
+  ]
+}
+
+TEST_DATA_PAGE_TWO = {
+  "pages": {
+    "current": 2,
+    "total": 2,
+    "next_page": ""
+  },
+  "events": [
+    {
+      "calendar_id": "cal_U9uuErStTG@EAAAB_IsAsykA2DBTWqQTf-f0kJw",
+      "event_uid": "evt_external_64008b1a4a41730f8d5c6057",
+      "summary": "Company Retreat 2",
+      "description": "",
+      "start": "2014-10-06",
+      "end": "2014-10-08",
+      "deleted": False,
+      "location": {
+        "description": "Mountains"
+      },
+      "participation_status": "needs_action",
+      "transparency": "opaque",
+      "event_status": "confirmed",
+      "categories": [],
+      "attendees": [
+        {
+          "email": "example@cronofy.com",
+          "display_name": "Example Person",
+          "status": "needs_action"
+        }
+      ],
+      "created": "2014-10-01T08:00:01Z",
+      "updated": "2014-10-01T09:24:16Z"
+    }
+  ]
+}
+
+NEXT_PAGE_GET_ARGS = {
+    'method': responses.GET, 
+    'url': '%s/%s/events/pages/08a07b034306679e' % (settings.API_BASE_URL, settings.API_VERSION),
+    'body': json.dumps(TEST_DATA_PAGE_TWO),
+    'status': 200,
+    'content_type':'application/json'
+}
 
 @pytest.fixture(scope="module")
 def client():
@@ -18,20 +97,20 @@ def test_all(client):
 
     :param CronofyClient client: CronofyClient instance with test data.
     """
-    pages = Pages(client=client, data=deepcopy(test_data.TEST_DATA_PAGE_ONE), data_type='events')
-    responses.add(**test_data.NEXT_PAGE_GET_ARGS)
+    pages = Pages(client=client, data=deepcopy(TEST_DATA_PAGE_ONE), data_type='events')
+    responses.add(**NEXT_PAGE_GET_ARGS)
     results = pages.all()
     assert len(results) == 2
-    assert results[0]['summary'] == test_data.TEST_DATA_PAGE_ONE['events'][0]['summary']
-    assert results[1]['summary'] == test_data.TEST_DATA_PAGE_TWO['events'][0]['summary']
+    assert results[0]['summary'] == TEST_DATA_PAGE_ONE['events'][0]['summary']
+    assert results[1]['summary'] == TEST_DATA_PAGE_TWO['events'][0]['summary']
 
 def test_current_page(client):
     """Test Pages.current_page() returns current page.
 
     :param CronofyClient client: CronofyClient instance with test data.
     """
-    pages = Pages(client=client, data=deepcopy(test_data.TEST_DATA_PAGE_ONE), data_type='events')
-    assert pages.current_page() == test_data.TEST_DATA_PAGE_ONE['events']
+    pages = Pages(client=client, data=deepcopy(TEST_DATA_PAGE_ONE), data_type='events')
+    assert pages.current_page() == TEST_DATA_PAGE_ONE['events']
 
 @responses.activate
 def test_fetch_next_page(client):
@@ -39,18 +118,18 @@ def test_fetch_next_page(client):
 
     :param CronofyClient client: CronofyClient instance with test data.
     """
-    pages = Pages(client=client, data=deepcopy(test_data.TEST_DATA_PAGE_ONE), data_type='events')
-    responses.add(**test_data.NEXT_PAGE_GET_ARGS)
+    pages = Pages(client=client, data=deepcopy(TEST_DATA_PAGE_ONE), data_type='events')
+    responses.add(**NEXT_PAGE_GET_ARGS)
     pages.fetch_next_page()
-    assert pages[0]['summary'] == test_data.TEST_DATA_PAGE_TWO['events'][0]['summary']
+    assert pages[0]['summary'] == TEST_DATA_PAGE_TWO['events'][0]['summary']
 
 def test_json(client):
     """Test Pages.json() returns raw json for page one.
 
     :param CronofyClient client: CronofyClient instance with test data.
     """
-    pages = Pages(client=client, data=deepcopy(test_data.TEST_DATA_PAGE_ONE), data_type='events')
-    assert pages.json() == test_data.TEST_DATA_PAGE_ONE
+    pages = Pages(client=client, data=deepcopy(TEST_DATA_PAGE_ONE), data_type='events')
+    assert pages.json() == TEST_DATA_PAGE_ONE
 
 @responses.activate
 def test_next(client):
@@ -58,27 +137,27 @@ def test_next(client):
 
     :param CronofyClient client: CronofyClient instance with test data.
     """
-    pages = Pages(client=client, data=deepcopy(test_data.TEST_DATA_PAGE_ONE), data_type='events')
-    responses.add(**test_data.NEXT_PAGE_GET_ARGS)
+    pages = Pages(client=client, data=deepcopy(TEST_DATA_PAGE_ONE), data_type='events')
+    responses.add(**NEXT_PAGE_GET_ARGS)
     results = []
     for item in pages:
         results.append(item)
     assert len(results) == 2
-    assert results[0]['summary'] == test_data.TEST_DATA_PAGE_ONE['events'][0]['summary']
-    assert results[1]['summary'] == test_data.TEST_DATA_PAGE_TWO['events'][0]['summary']
+    assert results[0]['summary'] == TEST_DATA_PAGE_ONE['events'][0]['summary']
+    assert results[1]['summary'] == TEST_DATA_PAGE_TWO['events'][0]['summary']
 
 def test___getitem__(client):
     """Test Pages.__getitem__() returns the item at the specified index.
 
     :param CronofyClient client: CronofyClient instance with test data.
     """
-    pages = Pages(client=client, data=deepcopy(test_data.TEST_DATA_PAGE_ONE), data_type='events')
-    assert pages[0]['summary'] == test_data.TEST_DATA_PAGE_ONE['events'][0]['summary']
+    pages = Pages(client=client, data=deepcopy(TEST_DATA_PAGE_ONE), data_type='events')
+    assert pages[0]['summary'] == TEST_DATA_PAGE_ONE['events'][0]['summary']
 
 def test___len__(client):
     """Test Pages.__len__() returns the length of the current page of data.
 
     :param CronofyClient client: CronofyClient instance with test data.
     """
-    pages = Pages(client=client, data=deepcopy(test_data.TEST_DATA_PAGE_ONE), data_type='events')
+    pages = Pages(client=client, data=deepcopy(TEST_DATA_PAGE_ONE), data_type='events')
     assert len(pages) == 1
