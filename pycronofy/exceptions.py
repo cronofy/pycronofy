2c72ee5b493211609e96e02d615235e3fc5c9dd0
Dan Pozmanter <dan.pozmanter@venuebook.com>
2016-05-06 11:08:03 -0400

    Cleaner exceptions & adding in header info with Authorization stripped out

--- pycronofy/exceptions.py
+++ pycronofy/exceptions.py
@@ -1,5 +1,6 @@
 class PyCronofyDateTimeError(Exception):
     """Exception class for datetime_utils improper argument."""
+
     def __init__(self, message, argument):
         """
         :param string message: Exception message.
@@ -11,24 +12,28 @@ class PyCronofyDateTimeError(Exception):
 
 class PyCronofyRequestError(Exception):
     """Wraps requests.exceptions.HTTPError for convenience and give a little more info in the message."""
+
     def __init__(self, request, response):
         """
         :param Request request: requests.Request.
         :param Response response: responses.Response.
         """
         body = ''
-        if request.method in ('POST', 'PUT') and request.body:
-            body = '\nPOST data: %s' % request.body
-        # Message leaves out request.headers for security reasons.
+        if request.method in ('POST', 'PUT', 'PATCH') and request.body:
+            body = '\nRequest Body: %s' % request.body
+        headers = request.headers
+        headers.pop('Authorization')
+        # Message leaves out request.headers['Authorization'] for security reasons.
         # This can be accessed via the exception:
-        # exception_instance.request.headers
-        message = 'Response: %(status_code)s: %(reason)s\nRequest: %(method)s %(url)s%(body)s%(text)s' % {
-            'status_code': response.status_code,
-            'reason': response.reason,
-            'text': ('\n%s' % response.text) if response.text else '',
-            'method': request.method,
-            'url': request.url,
+        # exception_instance.request.headers['Authorization']
+        message = 'Response: %(status_code)s: %(reason)s\nRequest: %(method)s %(url)s\nRequest Headers:%(headers)s%(body)s%(content)s' % {
             'body': body,
+            'content': ('\nResponse Content:\n%s' % response.content) if response.content else '',
+            'headers': headers,
+            'method': request.method,
+            'reason': response.reason,
+            'status_code': response.status_code,
+            'url': request.url,
         }
         super(PyCronofyRequestError, self).__init__(message)
         self.message = message
@@ -37,6 +42,7 @@ class PyCronofyRequestError(Exception):
 
 class PyCronofyValidationError(Exception):
     """Exception class for validation errors with client.Client methods."""
+
     def __init__(self, message, method, fields=None, values=None):
         """
         :param string message: Exception message.
