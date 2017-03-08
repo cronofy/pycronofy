26e72b9610e5662f8428648dd7e4c3ccdb5c2e9d
Jonathan S. Katz <jonathan.katz@excoventures.com>
2017-03-08 11:25:26 -0500

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from distutils.core import setup
 setup(
     name='PyCronofy',
-    version='1.0.0',
+    version='1.0.1',
     description='Python library for Cronofy',
-    author='Venuebook Dev Team (Dan Pozmanter et al)',
+    author='VenueBook',
     author_email='dev@venuebook.com',
     url='https://github.com/venuebook/pycronofy',
     packages=['pycronofy']
