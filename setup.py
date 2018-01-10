95003add08f734b70bce3aebdd740711c6b75e9e
Stephen Binns <stephen.binns@gmail.com>
2018-01-10 10:32:49 +0000

    Bump version and update urls

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from distutils.core import setup
 setup(
     name='PyCronofy',
-    version='1.0.1',
+    version='1.1.0',
     description='Python library for Cronofy',
     author='VenueBook',
     author_email='dev@venuebook.com',
-    url='https://github.com/venuebook/pycronofy',
+    url='https://github.com/cronofy/pycronofy',
     packages=['pycronofy']
 )
