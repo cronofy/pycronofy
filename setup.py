4ecf1931fe9fcc50f3bd57193e11547fd5d401fb
Stephen Binns <stephen.binns@gmail.com>
2018-01-10 12:40:50 +0000

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 setup(
     name='PyCronofy',
-    version='1.1.0',
+    version='1.1.1',
     description='Python library for Cronofy',
     author='VenueBook',
     author_email='dev@venuebook.com',
