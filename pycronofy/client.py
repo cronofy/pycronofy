452d829685814785f55567e18ef9f11b70c5151f
Chris Nevett <chris.nevett@cronofy.com>
2021-07-08 12:11:42 +0100

    tweak docstring and method name, add test around provider_name

--- pycronofy/client.py
+++ pycronofy/client.py
@@ -965,11 +965,13 @@ class Client(object):
         # compare_digest used to reduce vulnerability to timing attacks
         return any(hmac.compare_digest(value.encode(), calculated) for value in hmac_list)
 
-    def conferencing_services_auth_link(self, redirect_uri, provider_name=None):
-        """Get an auth link to redirect the user to so they can authorize with a conferencing provider
+    def get_conferencing_services_auth_link(self, redirect_uri, provider_name=None):
+        """Get a URL to direct the user to so they can authorize with a conferencing provider
 
-        :param string redirect_uri: URL to redirect the user to after auth.
-        :param string provider_name: Restrict possible conferencing providers. Must be one of 'ms_teams', 'go_to', 'zoom'.
+        :param string redirect_uri: URL to redirect the user to after authorization.
+        :param string provider_name: Optional provider identifier to pre-select for the user.
+         Refer to the documentation at https://docs.cronofy.com/developers/api/conferencing-services/authorization for possible values.
+        :return: the URL to direct the user to
         """
         data = {"redirect_uri": redirect_uri}
         if provider_name:
--- pycronofy/tests/test_conferencing_services.py
+++ pycronofy/tests/test_conferencing_services.py
@@ -24,7 +24,7 @@ def client():
 def test_conferencing_services_auth_link(client):
     def request_callback(request):
         payload = json.loads(request.body)
-        assert payload["redirect_uri"]
+        assert payload["redirect_uri"] == "https://redirect.here.please/"
 
         return (200, {}, json.dumps(TEST_CONFERENCING_SERVICES_AUTHORIZATION_RESPONSE))
 
@@ -36,6 +36,28 @@ def test_conferencing_services_auth_link(client):
         content_type="application/json",
     )
 
-    auth_link = client.conferencing_services_auth_link("https://redirect.here.please/")
+    auth_link = client.get_conferencing_services_auth_link("https://redirect.here.please/")
+
+    assert auth_link == TEST_CONFERENCING_SERVICES_AUTHORIZATION_URL
+
+
+@responses.activate
+def test_conferencing_services_auth_link_with_provider(client):
+    def request_callback(request):
+        payload = json.loads(request.body)
+        assert payload["redirect_uri"] == "https://redirect.here.please/"
+        assert payload["provider_name"] == "zoom"
+
+        return (200, {}, json.dumps(TEST_CONFERENCING_SERVICES_AUTHORIZATION_RESPONSE))
+
+    responses.add_callback(
+        responses.POST,
+        url="%s/%s/conferencing_service_authorizations"
+        % (settings.API_BASE_URL, settings.API_VERSION),
+        callback=request_callback,
+        content_type="application/json",
+    )
+
+    auth_link = client.get_conferencing_services_auth_link("https://redirect.here.please/", provider_name="zoom")
 
     assert auth_link == TEST_CONFERENCING_SERVICES_AUTHORIZATION_URL
